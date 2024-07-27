9d0a8c2035bff8c10e7efe563d073d73479de62f
kay_wu <kay_wu@edge-core.com>
2024-07-27 10:18:58 +0800

    Stage 3: Site Header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
+import { SiteHeader } from "@/components/site-header";
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
 
@@ -23,7 +24,12 @@ export default function RootLayout({
           "min-h-screen bg-background font-sans antialiased",
           inter.variable
         )}
-      >{children}</body>
+      >
+        <div className="relative flex min-h-dvh flex-col bg-background">
+          <SiteHeader />
+          <main className="flex-1">{children}</main>
+        </div>
+      </body>
     </html>
   );
 }
---
+++ src/config/site.ts
@@ -0,0 +1,13 @@
+export const siteConfig = {
+  name: "JhenHaoBolg",
+  url: "https://example.com",
+  description: "Nextjs 14 blog using velite, tailwind and shadcn",
+  author: "JhenHao-Coding",
+  links: {
+    twitter: "",
+    github: "",
+    personalSite:""
+  }
+}
+
+export type SiteConfig = typeof siteConfig
