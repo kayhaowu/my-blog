98c4da584299b4a79921b6abc1f564ddffb382e4
kay_wu <kay_wu@edge-core.com>
2024-07-28 22:24:05 +0800

    Stage 8: Custom Components in MDX

--- src/components/mdx-components.tsx
+++ src/components/mdx-components.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import * as runtime from "react/jsx-runtime";
 import { cn } from "@/lib/utils";
+import { Callout } from "./callout";
 
 const useMDXComponent = (code: string) => {
   const fn = new Function(code);
@@ -9,6 +10,16 @@ const useMDXComponent = (code: string) => {
 
 const components = {
   Image,
+  Callout,
+  h1: ({ className, ...props }) => (
+    <h1
+      className={cn(
+        "mt-2 scroll-m-20 text-4xl font-bold tracking-tight",
+        className
+      )}
+      {...props}
+    />
+  ),
 };
 
 interface MdxProps {
